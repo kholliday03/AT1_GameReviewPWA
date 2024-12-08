6a86901e131136585ebf64cc241cebfd9f428ffd
kai.holliday3@education.nsw.gov.au <kai.holliday3@education.nsw.gov.au>
2024-12-08 21:29:04 +1100

    Commit XI - Modified style.css, setup.sql, reinitialised database.db.

--- database/setup.sql
+++ database/setup.sql
@@ -7,15 +7,17 @@ CREATE TABLE IF NOT EXISTS accounts(
 CREATE TABLE IF NOT EXISTS posts(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
+    game_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    rating FLOAT NOT NULL,
+    rating INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE IF NOT EXISTS games(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER NOT NULL,
     game_title TEXT NOT NULL,
     year_released INTEGER NOT NULL
 );
